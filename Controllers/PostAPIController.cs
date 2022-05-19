b22444bb3005ca4013f3c84dd0fba4fa91a60e0f
AndreasHolmAndersen <andreas.holm.andersen@gmail.com>
2022-05-19 10:14:47 +0200

    scaffolded api controller, AND IT WORKS

---
+++ Controllers/PostAPIController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using cbsStudents.Data;
+using cbsStudents.Models.Entities;
+
+namespace cbsStudents.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostAPIController : ControllerBase
+    {
+        private readonly CbsStudentsContext _context;
+
+        public PostAPIController(CbsStudentsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PostAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Post>>> GetPost()
+        {
+            return await _context.Post.ToListAsync();
+        }
+
+        // GET: api/PostAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Post>> GetPost(int id)
+        {
+            var post = await _context.Post.FindAsync(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return post;
+        }
+
+        // PUT: api/PostAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPost(int id, Post post)
+        {
+            if (id != post.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(post).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PostAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Post>> PostPost(Post post)
+        {
+             post.Created = DateTime.Now;
+            _context.Post.Add(post);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPost", new { id = post.Id }, post);
+        }
+
+        // DELETE: api/PostAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            var post = await _context.Post.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            _context.Post.Remove(post);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.Post.Any(e => e.Id == id);
+        }
+    }
+}
